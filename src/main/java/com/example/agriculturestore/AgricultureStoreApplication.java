aa5a60e3d9e323a6217465725cca9e30197111f6
mohammadrezaaliabadi <mohammadrezaaliabadi9@gmail.com>
2022-06-21 09:10:11 -0700

    initial projects.

---
+++ src/main/java/com/example/agriculturestore/AgricultureStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.agriculturestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgricultureStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgricultureStoreApplication.class, args);
+    }
+
+}
